81e596cb1268620697ed7eb99cfd38f6b1ba6e29
wuchanghua <1453346832@qq.com>
2023-04-05 11:50:07 +0800

    npm init

---
+++ src/components/lottie-view.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="">
+    <div :style="style" ref="lavContainer"></div>
+    </div>
+</template>
+  
+  <script>
+  import lottie from 'lottie-web'
+  
+  export default {
+    props: {
+      options: {
+        type: Object,
+        required: true,
+      },
+      height: Number,
+      width: Number,
+    },
+  
+    computed: {
+      style() {
+        return {
+          width: this.width ? `${this.width}px` : '100%',
+          height: this.height ? `${this.height}px` : '100%',
+        }
+      },
+    },
+  
+    mounted() {
+      this.anim = lottie.loadAnimation({
+        container: this.$refs.lavContainer,
+        renderer: 'svg',
+        loop: this.options.loop !== false,
+        autoplay: this.options.autoplay !== false,
+        animationData: this.options.animationData,
+      })
+      this.$emit('animCreated', this.anim)
+    },
+    
+    unmounted () {
+      this.anim && this.anim.destroy()
+    }
+  }
+  </script>
+  
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+      <div class="ani-box">
+        <cente-lottie :width="300" :height="300"></cente-lottie>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import centeLottie from '@/components/centeLottie'
+  export default  defineComponent({
+    components: {
+      centeLottie
+    }
+  })
+</script>
+
+
+<style scoped lang='less'>
+
+</style>
